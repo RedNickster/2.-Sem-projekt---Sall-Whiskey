44e005c43601bf2c6c7ca20bf745b4b3ff9e6e38
Luca <lucavadstruppetersen@gmail.com>
2026-04-30 10:43:54 +0200

    Cask og Distillation attribut+constructor

--- src/model/Cask.java
+++ src/model/Cask.java
@@ -1,4 +1,17 @@
 package model;
 
 public class Cask {
+    private int id;
+    private int liters;
+    private String previousLiquids;
+    private String countryOfOrigin;
+    private String supplier;
+
+    public Cask(int id, int liters, String previousLiquids, String countryOfOrigin, String supplier) {
+        this.id = id;
+        this.liters = liters;
+        this.previousLiquids = previousLiquids;
+        this.countryOfOrigin = countryOfOrigin;
+        this.supplier = supplier;
+    }
 }
