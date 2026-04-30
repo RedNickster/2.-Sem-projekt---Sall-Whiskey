41209b9dabc961bd3b03b3d978974047053fd42b
LPRosendahl <lucasrosendahl02@gmail.com>
2026-04-30 10:46:09 +0200

    Attributter og constructor i Cask

--- src/model/Cask.java
+++ src/model/Cask.java
@@ -1,4 +1,15 @@
 package model;
 
 public class Cask {
+    private int id;
+    private int liters;
+    private String previousLiquids;
+    private String supplier;
+
+    public Cask(int id, int liters, String previousLiquids, String supplier) {
+        this.id = id;
+        this.liters = liters;
+        this.previousLiquids = previousLiquids;
+        this.supplier = supplier;
+    }
 }
